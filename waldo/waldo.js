8616359a9425c2e8c7ae278f16a0ffee3ef872d5
Soma Jankovics-Eros <soma.jankovicseros@powerhub.com>
2017-11-02 14:46:22 +0000

    first

---
+++ waldo/waldo.js
@@ -0,0 +1,15 @@
+waldo.js
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found();   // execute callback
+    }
+  }
+}
+
+function actionWhenFound() {
+  console.log("Found him!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
